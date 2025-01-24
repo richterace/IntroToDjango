918558a61e49a2b32c3d871e27c2453fa2ae0efd
richterace <villasace098@gmail.com>
2025-01-24 15:20:16 +0800

    db interaction implemented

--- mysite/initial/urls.py
+++ mysite/initial/urls.py
@@ -3,6 +3,5 @@ from django.urls import path
 from . import views #import fro mthe directory we are in
 
 urlpatterns = [
-    path("", views.index, name="index"),
-    path("v1/", views.v1, name="v1"),
+    path("<int:id>", views.index, name="index"),
 ]
--- mysite/initial/views.py
+++ mysite/initial/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import ToDoList, Item
 # Create your views here.
 
-def index(response):
-    return HttpResponse("<h1>Hello, Ace</h1>")
+def index(response, id):
+    ls = ToDoList.objects.get(id=id)
+    items = ls.item_set.get(id=1)
+    return HttpResponse("<h1>%s</h1><br></br><p>%s</p>" %(ls.name, str(items.text)))
 
-def v1(response):
-    return HttpResponse("<h1>View 1</h1>")
 
